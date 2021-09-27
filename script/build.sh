cff4ae614b511d6038fbbc9c7132186a58f7bf5d
vsoch <vsoch@users.noreply.github.com>
2021-09-27 15:14:02 -0600

    remove extra validation
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- script/build.sh
+++ script/build.sh
@@ -5,5 +5,4 @@ set -e
 gem install html-proofer
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --check-html --check-sri --url-ignore '/fonts.gstatic.com/'
-bundle exec script/validate-html
 gem build radiuss-docs-jekyll.gemspec
