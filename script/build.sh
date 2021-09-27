2a66b1a13e57bcf5221f0359efb8b0145ba1af99
vsoch <vsoch@users.noreply.github.com>
2021-09-27 15:10:26 -0600

    need to install html proofer
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+gem install html-proofer
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --check-html --check-sri --url-ignore '/fonts.gstatic.com/'
 bundle exec script/validate-html
